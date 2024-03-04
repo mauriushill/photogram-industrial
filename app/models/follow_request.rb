aa4523768e861567a31cdd9f45701b028a253183
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-03-04 15:07:19 +0000

    Added enum method for follow request.

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,4 +22,6 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  enum status: { pending: "pending", rejeceted: "rejected", accepted: "accepted" }
 end
