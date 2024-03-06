b8ae17e7ca0bd8bf3c968fe9dafac0a7504603a4
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-03-06 21:48:54 +0000

    Added commenting.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: :show
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
- 
+
+  get "/:username" => "users#show"
 end
